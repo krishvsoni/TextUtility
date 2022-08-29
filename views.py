92df67bea2da884822b7be4b47977af292e45c02
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2022-08-30 01:07:31 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,81 @@
+# KRISH SONI
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charcount = request.POST.get('charcount', 'off')
+    wordcount = request.POST.get('wordcount', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!="\r":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+
+    if (charcount == "on"):
+        analyzed = len(djtext)
+
+        params = {'purpose': 'Character Count ', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if (wordcount =="on"):
+
+       analyzed = len(djtext.split())
+       print(str(analyzed))
+       djtext=analyzed
+
+       params = {'purpose': 'Word Count', 'analyzed_text': analyzed}
+       djtext = analyzed
+
+
+
+
+
+
+    if(removepunc != "on" and newlineremover!="on" and extraspaceremover!="on" and fullcaps!="on" and charcount!="on" and wordcount!="on"):
+        return HttpResponse("<p><h1><b><u> ERROR 404 👻 </u></b></1></p> <b> Please select any operation and try again</b>")
+
+    return render(request, 'analyze.html', params)
