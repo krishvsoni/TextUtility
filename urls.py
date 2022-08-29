e9464f5189a5ae72e94f88e7722a97b92f81e789
KRISH SONI <67964054+krishvsoni@users.noreply.github.com>
2022-08-30 01:08:27 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
-
+# KRISH SONI
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
